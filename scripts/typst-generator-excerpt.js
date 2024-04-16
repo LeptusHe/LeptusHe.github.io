f66bf135f73a1940fcbae8fc582ca16b6c31c0cf
LeptusHe <heweidong526@gmail.com>
2024-04-17 01:07:07 +0800

    增加从文本中提取abstract的功能

--- scripts/typst-generator-excerpt.js
+++ scripts/typst-generator-excerpt.js
@@ -1,4 +1,5 @@
 const TypstCli = require("./typst-cli");
+const {truncate} = require("hexo/lib/plugins/helper/format");
 
 
 class TypstExcerptGenerator {
@@ -15,6 +16,25 @@ class TypstExcerptGenerator {
         });
     }
 
+    findAbstractInContent(content) {
+        const lines = content.split(/\n/).filter(line => line.trim() !== '');
+        const index = lines.findIndex(line => line.trim().startsWith('='));
+        if (index >= 0) {
+            let abstract = lines.slice(index + 1, lines.length).join("\n");
+            return this.truncateAbstract(abstract, 120);
+        } else {
+            return null;
+        }
+    }
+
+    truncateAbstract(text, maxLen) {
+        if (text.length > maxLen) {
+            return text.substring(0, maxLen);
+        } else {
+            return text;
+        }
+    }
+
     processPost(post) {
         //console.log(`processPost: ${post.path}, ${post.content}`);
         if (post.source === undefined || post.source == null) {
@@ -26,10 +46,15 @@ class TypstExcerptGenerator {
         }
 
         console.warn(`start to query abstract ${post.source}`)
-        this.typstCli.queryMetadata(post, "abstract", (post, result) => {
+        const excerpt = this.typstCli.queryMetadata(post, "abstract", (post, result) => {
             post.excerpt = result;
-            this.info(`succeed to write excerpt ${post.excerpt}`);
+            this.info(`succeed to find abstract with excerpt tag: excerpt=${post.excerpt}, source=${post.source}`);
         });
+
+        if (excerpt === undefined || excerpt == null) {
+            post.excerpt = this.findAbstractInContent(post._content);
+            this.info(`succeed to find abstract in content: excerpt=${post.excerpt.slice(0, 10)}, source=${post.source}`);
+        }
 
         return {
             path: post.path,
